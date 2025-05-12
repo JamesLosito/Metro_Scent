7c0eaf063373933bb58f5a53ed71455b66894fc7
Arjay Lagunoy <arjaylascano.lagunoy@bicol-u.edu.ph>
2025-05-13 00:37:23 +0800

    up

--- routes/web.php
+++ routes/web.php
@@ -105,4 +105,3 @@ use App\Http\Controllers\Auth\AuthenticatedSessionController;
 Route::get('login', [AuthenticatedSessionController::class, 'create'])->middleware('guest')->name('login');
 Route::post('login', [AuthenticatedSessionController::class, 'store'])->middleware('guest');
 Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])->middleware('auth')->name('logout');
-
